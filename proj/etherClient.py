9ba583996388b5340df1d9f2a92d0414d166fadd
david <d.zanella@campus.fct.unl.pt>
2022-11-17 14:41:17 +0000

    upload

--- proj/etherClient.py
+++ proj/etherClient.py
@@ -31,14 +31,13 @@ if __name__ == "__main__":
     frame = et.build_frame(target_addr, my_addr, sys.argv[1])
     sck.send(frame)
     print("Find?")
-    execw=True
-    while execw:
+    while True:
         recv_frame = sck.recv(et.MAX_FRAME_SIZE)
         my_addr_in_frame, sender_addr, _, msg = et.extract_frame(recv_frame)
         if(sender_addr != my_addr):
             print("Message: ", msg)
             print("Server address: ", et.bytes_ether_addr_to_string(sender_addr))
-            execw = False
+            break
 
     
     sck.close()
