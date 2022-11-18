debe24a51cb0afe10c8b9de4a79e776384bd0cc1
AlexIova <iovaalexandru37@gmail.com>
2022-11-18 11:38:46 +0100

    Refactoring

--- proj/etherClient.py
+++ proj/etherClient.py
@@ -15,31 +15,33 @@ if len(sys.argv) < 2:
 if __name__ == "__main__":
     #creates RAW ethernet socket
     sck = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(et.ETH_P_ALL))
-    #Binding ethernet interface to the socket
-    sck.bind(("eth0", 0))
-    
-    #eth0 - default name of the ethernet interface in UNIX
-    my_addr = et.get_self_addr(sck, 'eth0')
 
-    #argv[1]: ip searched
-    print("ip looking for: ", sys.argv[1])
+    try: 
+        #Binding ethernet interface to the socket
+        sck.bind(("eth0", 0))
+        
+        #eth0 - default name of the ethernet interface in UNIX
+        my_addr = et.get_self_addr(sck, 'eth0')
 
-
-    target_addr = et.ether_addr_to_bytes("ff:ff:ff:ff:ff:ff")
-    
-    #builds frame to send
-    frame = et.build_frame(target_addr, my_addr, sys.argv[1])
-    sck.send(frame)
-    print("Find?")
-    
-    while True:
-        recv_frame = sck.recv(et.MAX_FRAME_SIZE)
-        my_addr_in_frame, sender_addr, _, msg = et.extract_frame(recv_frame)
-        if(sender_addr != my_addr):
-            print("Message: ", msg)
-            print("Server address: ", et.bytes_ether_addr_to_string(sender_addr))
-            break
+        #argv[1]: ip searched
+        print("ip looking for: ", sys.argv[1])
 
 
-    
-    sck.close()
+        target_addr = et.ether_addr_to_bytes("ff:ff:ff:ff:ff:ff")
+        
+        #builds frame to send
+        frame = et.build_frame(target_addr, my_addr, sys.argv[1])
+        sck.send(frame)
+        print("Found?")
+        
+        while True:
+            recv_frame = sck.recv(et.MAX_FRAME_SIZE)
+            my_addr_in_frame, sender_addr, _, msg = et.extract_frame(recv_frame)
+            if(sender_addr != my_addr):
+                print("Message: ", msg)
+                print("Server address: ", et.bytes_ether_addr_to_string(sender_addr))
+                break
+
+
+    except KeyboardInterrupt:
+        sck.close()
--- proj/etherServer.py
+++ proj/etherServer.py
@@ -4,33 +4,38 @@
 
 import socket
 import ethernetTools as et
+import sys
 
 if __name__ == "__main__":
     sck = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(et.ETH_P_ALL))  #creates RAW ethernet socket
-    sck.bind(("eth0", 0))     #Binding ethernet interface to the socket
-    
-    #eth0 - default name of the ethernet interface in UNIX
-    my_addr = et.get_self_addr(sck, 'eth0')
-    #prints ethernet address in human-readable format
-    print('Ethernet address:', et.bytes_ether_addr_to_string(my_addr))
 
-    my_ip=et.get_self_ip_addr(sck, 'eth0')
-    print('IP: ', et.bytes_ip_addr_to_string(my_ip))
+    try: 
+        sck.bind(("eth0", 0))     #Binding ethernet interface to the socket
+        
+        #eth0 - default name of the ethernet interface in UNIX
+        my_addr = et.get_self_addr(sck, 'eth0')
+        #prints ethernet address in human-readable format
+        print('Ethernet address:', et.bytes_ether_addr_to_string(my_addr))
 
-    while True:           
-        #Keeps waiting for ethernet frames and prints them
-        print("Waiting for an ethernet frame...")
-        #receives an ethenet frame
-        recv_frame = sck.recv(et.MAX_FRAME_SIZE)
-    
-        #extracts content of the frame
-        my_addr_in_frame, sender_addr, _, msg = et.extract_frame(recv_frame)
+        my_ip = et.get_self_ip_addr(sck, 'eth0')
+        print('IP: ', et.bytes_ip_addr_to_string(my_ip))
 
-        if(msg == et.bytes_ip_addr_to_string(my_ip)):  
-            print("Ethernet frame received!")
-            msgrep="Hi, I'm the Server you are looking for!"
-            frame = et.build_frame(sender_addr, my_addr, msgrep)
-            sck.send(frame)
-        print()
-    
-    sck.close()
+        while True:           
+            #Keeps waiting for ethernet frames and prints them
+            print("Waiting for an ethernet frame...")
+            #receives an ethenet frame
+            recv_frame = sck.recv(et.MAX_FRAME_SIZE)
+        
+            #extracts content of the frame
+            my_addr_in_frame, sender_addr, _, msg = et.extract_frame(recv_frame)    # (my_addr_in_frame, sender_addr, ether_type, data)
+
+            if(msg == et.bytes_ip_addr_to_string(my_ip)):  
+                print("Ethernet frame received!")
+                msgrep = "Hi, I'm the Server you are looking for!"
+                frame = et.build_frame(sender_addr, my_addr, msgrep)
+                sck.send(frame)
+            print()
+
+    except KeyboardInterrupt:
+        sck.close()
+        sys.exit(1)
